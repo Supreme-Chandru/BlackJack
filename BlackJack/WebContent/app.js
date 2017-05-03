a5373b08d04d666d79e14f6579b1317591c57ac9
Supreme <supreme.halagurchandras@mavs.uta.edu>
2017-05-03 03:22:51 -0500

    Initial Setup and Overall Idea Comment

---
+++ BlackJack/WebContent/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+/*
+ * Using BackBone
+ * 
+ * Create Prototype/Abstract of card game and then extend it to BLackJack Card Game 
+ * 
+ * Suit - shape
+ * Abstract CArd has Facevalue and Suit. It should have some status. Facevalue changes based on game.
+ * 
+ * Deck is a collection of CArd
+ * 
+ * Deck can dealhand, dealcard
+ * 
+ * Abstract Hand - is the player cards, keep track of score
+ * 
+ * 
+ * Create models using backbomes
+ * Extend to
+ * -BlackJackGame
+ * -BlackJackCard
+ * -BlackJackScore
+ * 
+ * Extra functions such as Soft17, isAce, isFaceCArd, is21, isBlackjack, busted
+ * 
+ */	
+
+	alert("Overall Idea");
+});
