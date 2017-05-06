544af6cabc9082a96e53ecce59d91c9ac9958c59
Supreme <supreme_hc2009@yahoo.co.in>
2017-05-06 08:49:35 -0500

    Player Model Player has hit and stand Action Player handles won, lost, bust Scenarios

--- BlackJack/WebContent/model/Player.js
+++ BlackJack/WebContent/model/Player.js
@@ -1,13 +1,49 @@
-define(['underscore','backbone'], function (_, BackBone) {
+define(['underscore','backbone','model/ScoreBoard','collection/BlackJackHand','model/Score'], function (_, BackBone, ScoreBoard, BlackJackHand, Score) {
 	
 
-	Player = BackBone.model.extend({
+	var Player = BackBone.Model.extend({
 		
 		initialize:function(){
-			this.gamesPlayed=0,
-			this.gamesWon=0,
-			this.gamesLost=0
+			
+			this.set("scoreBoard",new ScoreBoard());
+			var blackJackHand = new BlackJackHand(null,{});
+			
+			this.set("hand",blackJackHand);
+			
+		},
+		hit:function(){
+			console.log(this.get("name")+" : ACTION --> HIT");
+			var dealer = this.get("dealer");
+			dealer.draw(this); //player asks dealer to draw the card for him
+		},
+		
+		stand:function(){
+			console.log(this.get("name")+" : ACTION --> STAND");
+			var dealer = this.get("dealer");
+			dealer.endPlayerRound(this);
+		},
+		bust:function(){
+			console.log(this.get("name")+" : STATUS --> BUSTED");
+			// dealer has busted him
+			lost();
+			
+		},
+		
+		won:function(){
+			console.log(this.get("name")+" : STATUS --> WON");
+			var scoreBoard = this.set("scoreBoard");
+			scoreBoard.incrementTotalWon();
+			scoreBoard.incrementTotalPlayed();
+		},
+		
+		lost:function(){
+			console.log(this.get("name")+" : STATUS --> LOST");
+			var scoreBoard = this.set("scoreBoard");
+			scoreBoard.incrementTotalPlayed();
 		}
+	
+		
+	
 	});
 
 
